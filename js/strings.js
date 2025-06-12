fad07a922fa3270ffbeb74e52625fac78b67439a
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-12 16:00:52 -0400

    strings.js update to show multiline strings

---
+++ js/strings.js
@@ -0,0 +1,11 @@
+const htmlSnippet = '<h1 class="headline">My Headline</h1'
+const message = "I'm a programmer!"
+
+//Backslash to tell string continues.
+
+const multiline =
+  'Hello, students. \
+Welcome to JavaScript Basics. \
+I hope you learn a lot!'
+
+console.log(multiline)
