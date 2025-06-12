45be68ff19ba94c0f90ac2ed7e29ad1f7e7a1cb1
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-12 17:02:28 -0400

    template literals introduction

--- js/template-literal.js
+++ js/template-literal.js
@@ -1,5 +1,8 @@
+//Use backticks instead of " or '
+//Can do almost any js inside ${}
+
 const name = prompt('What is your name?')
 
-const message = 'Hello' + name
+const message = `Hello, ${name}. Welcome to my music site. I'm happy that you came to visit, ${name}. Please come again and listen to more music!`
 
 console.log(message)
