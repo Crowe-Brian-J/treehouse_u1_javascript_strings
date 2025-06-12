f208a8513b4f2d5cf0956a3bc5ef6f70ea63cb92
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-12 16:32:21 -0400

    use transform.js to show string changes

---
+++ js/transform.js
@@ -0,0 +1,8 @@
+const passphrase = 'Open Sesame'
+
+//.toUpperCase and .toLowerCase to make all characters upper- or lowercase
+
+console.log(passphrase.toLowerCase())
+console.log(passphrase.toUpperCase())
+
+console.log(passphrase)
