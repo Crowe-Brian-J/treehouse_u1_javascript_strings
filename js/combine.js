3f30d0dbc337854f83ace6d9a7f7cbe4b5e91c88
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-12 16:53:41 -0400

    string concatenation exercise

---
+++ js/combine.js
@@ -0,0 +1,9 @@
+//String concatenation
+
+const name = prompt('What is your name?')
+let message = 'Hello, ' + name + '. Welcome to my music site.'
+message += " I'm so happy that you came by to visit, "
+message += name
+message += '. Feel free to come again to listen to more music!'
+
+console.log(message)
